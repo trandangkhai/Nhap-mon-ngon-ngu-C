7dbf6b3cf908a8623e5a6234479ba7c12f05143c
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2020-12-10 16:12:32 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,16 @@
+int main(){
+//khai bao bien a, b
+int a, b;
+printf("nhap a tu ban phim :",a);
+scanf("%d", &a);
+printf("nhap b tu ban phim :",b);
+scanf("%d", &b);
+if(a > b){
+printf("a>b",a,b);
+}else if( a== b){
+printf("a==b",a,b);
+}else if(a < b){
+printf("a<b ",a,b);
+}
+return 0;
+}soánh.c
