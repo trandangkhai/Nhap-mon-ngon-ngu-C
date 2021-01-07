5851dd83f7ab18b99be91ac851b8303eac3ee421
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2021-01-07 16:41:44 +0700

    Create kt_01.c

---
+++ Bài tập 3/kt_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(){
+	int a ;
+
+  printf("Bang cuu chuong \n\n\n");
+	
+  printf("Bang cuu chuong 2:\n\n 2 x 1 = 2\n 2 x 2 = 4\n 2 x 3 = 6\n\n");
+	
+  printf("Bang cuu chuong 3:\n\n 3 x 1 = 3\n 3 x 2 = 6\n 3 x 3 = 9\n\n");
+	
+  printf("Bang cuu chuong 4:\n\n 4 x 1 = 4\n 4 x 2 = 8\n 4 x 3 = 12\n\n");
+	
+	return 0;
+}
