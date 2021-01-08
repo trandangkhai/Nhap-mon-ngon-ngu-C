f63c3d9ad2773a9776116d336cb0c4ec5b6c9cf0
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2021-01-08 10:38:24 +0700

    Create kt03.c

---
+++ Bài tập 3/kt03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int i ; 
+  for (int i =1 ;i<=20;i=i+1  ) {
+    if (i==5) ||  (i==11) || (i==15)
+
+  
+  continue ;
+    printf ("%d",i);
+
+
+  }
+
+
+  return 0;
+}
