a7e544b4a4f463e36fbddf5f00c0b47569da6013
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2021-01-23 09:24:07 +0700

    Create TH.02.c

---
+++ Bài tập 5/TH.02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int t, z;
+  int c = 0;
+  printf("/n nhap t:");
+  scanf("%d",&t);
+  for(z = 1; z <= t;z++){
+    c = c + z;
+  }
+  printf("/n tong 1 + 2 +...+%d",c);
+  return 0;
+}
