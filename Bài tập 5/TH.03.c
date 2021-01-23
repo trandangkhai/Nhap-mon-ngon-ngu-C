2d2e4be522f5aab8fa33f82c425195a799b20ec1
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2021-01-23 10:34:28 +0700

    Create TH.03.c

---
+++ Bài tập 5/TH.03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+int so_lon_nhat(int a,int b,int c);
+int a , b, c;
+printf("nhap so a:");
+scanf("%d",&a);
+printf("nhap so b:");
+scanf("%d",&b);
+printf("nhap so c:");
+scanf("%d",&c);
+if (a > b && a > c)
+ printf("so lon nhat a");
+else if (b > a && b > c)
+printf("so lon nhat b");
+else
+ printf(" so lon nhat la c");
+}
+
+int so_lon_nhat(int a,int b,int c){
+
+return 0;
+}
