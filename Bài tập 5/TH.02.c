683ef6a63827bd08bbe94ae6e75f744b3bf5711c
trandangkhai <72543529+trandangkhai@users.noreply.github.com>
2021-01-23 10:08:31 +0700

    Update TH.02.c

--- Bài tập 5/TH.02.c
+++ Bài tập 5/TH.02.c
@@ -3,11 +3,11 @@
 int main() {
   int t, z;
   int c = 0;
-  printf("/n nhap t:");
+  printf("\n nhap t:");
   scanf("%d",&t);
   for(z = 1; z <= t;z++){
     c = c + z;
   }
-  printf("/n tong 1 + 2 +...+%d",c);
+  printf("\n tong 1 + 2 +...+%d",c);
   return 0;
 }
